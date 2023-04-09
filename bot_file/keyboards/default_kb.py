2ee075cd6c73c6f145a64384adc63c7619fb3b3f
ddosm <ddosmukhambetov@gmail.com>
2023-04-10 01:17:35 +0600

    Добавлена новая кнопка регистрации

--- bot_file/keyboards/default_kb.py
+++ bot_file/keyboards/default_kb.py
@@ -4,5 +4,6 @@ markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
 btn_1 = KeyboardButton('Помощь ⭐️')
 btn_2 = KeyboardButton('Описание 📌')
 btn_3 = KeyboardButton('Каталог 🛒')
+btn_4 = KeyboardButton('Регистрация ✌️')
 
-markup.add(btn_1).insert(btn_2).add(btn_3)
+markup.add(btn_1).insert(btn_2).add(btn_3).insert(btn_4)
