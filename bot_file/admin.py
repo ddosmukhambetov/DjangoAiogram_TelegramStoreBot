bcb3813b34c812b189e243d566bc6c62ed615392
ddosm <ddosmukhambetov@gmail.com>
2023-04-10 01:15:13 +0600

    Сделана регистрация модели TelegramUser как TelegramUserAdmin

--- bot_file/admin.py
+++ bot_file/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, TelegramUser
 
 
 @admin.register(Product)
@@ -14,3 +14,11 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'created_at']
     list_display_links = ['id', 'name']
     search_fields = ['id', 'name']
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_login', 'registered_at', 'is_registered']
+    list_display_links = ['id', 'user_login']
+    search_fields = ['id', 'user_login', 'registered_at']
+    readonly_fields = ['user_password']
