d483230d0c7d8371dbc0900e7a1fba89126fd46d
ddosm <ddosmukhambetov@gmail.com>
2023-04-10 21:33:07 +0600

    Доработана клавиатура

--- bot_file/keyboards/sign_inup_kb.py
+++ bot_file/keyboards/sign_inup_kb.py
@@ -5,5 +5,6 @@ markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
 
 btn_1 = KeyboardButton('Регистрация ✌️')
 btn_2 = KeyboardButton('Войти 👋')
+btn_3 = KeyboardButton("Забыли пароль?")
 
 markup.add(btn_1).add(btn_2)
