24db1e0d80c6b3a788c945856d0cb22170eaeffa
ddosm <ddosmukhambetov@gmail.com>
2023-04-10 23:53:31 +0600

    Добавлена новая строка

--- bot_file/states/forgot_password_state.py
+++ bot_file/states/forgot_password_state.py
@@ -4,3 +4,4 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class ForgotPasswordState(StatesGroup):
     user_login = State()
     user_password = State()
+    user_password_2 = State()
