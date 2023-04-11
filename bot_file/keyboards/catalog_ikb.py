a90426c781155ba89e6e67a5bb4239ae814684ad
ddosm <ddosmukhambetov@gmail.com>
2023-04-12 00:25:25 +0600

    Сделана инлайн клавиатура для подкатегории

--- bot_file/keyboards/catalog_ikb.py
+++ bot_file/keyboards/catalog_ikb.py
@@ -1,9 +1,10 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
 from asgiref.sync import sync_to_async
-from ..models import Category
+from ..models import Category, SubCategory
 
 category_cb = CallbackData('category', 'id', 'action')
+subcategory_cb = CallbackData('subcategory', 'id', 'action')
 
 
 @sync_to_async
@@ -12,5 +13,16 @@ def get_categories():
     markup = InlineKeyboardMarkup(row_width=1)
     for category in categories:
         markup.add(InlineKeyboardButton(text=category.name,
-                                        callback_data=category_cb.new(id=category.id, action='view')))
+                                        callback_data=category_cb.new(id=category.id, action='view_categories')))
     return markup
+
+
+@sync_to_async
+def get_subcategories(cat_id):
+    subcategories = SubCategory.objects.filter(subcategory_category_id=cat_id)
+    subcategory_markup = InlineKeyboardMarkup(row_width=2)
+    for subcategory in subcategories:
+        subcategory_markup.add(InlineKeyboardButton(text=subcategory.name,
+                                                    callback_data=subcategory_cb.new
+                                                    (id=subcategory.id, action='view_subcategories')))
+    return subcategory_markup
