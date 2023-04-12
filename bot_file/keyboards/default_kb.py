82c474984c09a12b412a2dc46214721d49bfea4c
ddosm <ddosmukhambetov@gmail.com>
2023-04-12 14:57:28 +0600

    Доработана клавиатура

--- bot_file/keyboards/default_kb.py
+++ bot_file/keyboards/default_kb.py
@@ -6,3 +6,8 @@ btn_2 = KeyboardButton('Описание 📌')
 btn_3 = KeyboardButton('Каталог 🛒')
 btn_4 = KeyboardButton('Админ 👑')
 markup.add(btn_1).insert(btn_2).add(btn_3).insert(btn_4)
+
+
+only_help_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+btn_1 = KeyboardButton('Помощь ⭐️')
+only_help_markup.add(btn_1)
