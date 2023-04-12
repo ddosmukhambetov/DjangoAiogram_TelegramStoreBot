1374db138fdd5442a48822f9404ee9570dee6248
ddosm <ddosmukhambetov@gmail.com>
2023-04-12 22:23:42 +0600

    Добавлены комментарии

--- bot_file/loader.py
+++ bot_file/loader.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import settings
 
+# Создаем нашего бота и диспатчер, MemoryStorage хранилище состояний
 bot = Bot(settings.TOKEN_API, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
