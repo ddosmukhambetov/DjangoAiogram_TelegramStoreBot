d2a5abe4e2758a55857cfde6187db38f069567da
ddosm <ddosmukhambetov@gmail.com>
2023-04-12 22:09:14 +0600

    Добавлен url для smart_selects

--- bot_file/urls.py
+++ bot_file/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from .views import index
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('chaining/', include('smart_selects.urls'))
 ]
