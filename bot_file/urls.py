7ce2efcfb2e814b83a6ad83521b74aaed6351a7d
ddosm <ddosmukhambetov@gmail.com>
2023-04-12 22:19:47 +0600

    Добавлены комментарии

--- bot_file/urls.py
+++ bot_file/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import index
 
+# Добавляем наши urls, точнее домашюю страницу, smart_selects для работы с зависимыми полями в Django админке
 urlpatterns = [
     path('', index),
     path('chaining/', include('smart_selects.urls'))
