539fa4d1b4a7d2b9532f3184ead8ae28ae929c19
ddosm <ddosmukhambetov@gmail.com>
2023-04-12 22:08:54 +0600

    Исправлена модель продкута связанный с подкатегорией. В Django адмике при добавлении товара он будет принимать категорию и подкатегорию. Когда он выберет категорию будут показываться подкатегории которые содержуть эту категорию. Сделана с помощью smart_selects

--- bot_file/models.py
+++ bot_file/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from smart_selects.db_fields import ChainedForeignKey
 
 
 class TelegramUser(models.Model):
@@ -16,28 +17,6 @@ class TelegramUser(models.Model):
         verbose_name_plural = 'Пользователи'
         db_table = 'telegram_users'
         ordering = ['-registered_at']
-
-
-class Product(models.Model):
-    photo = models.ImageField(verbose_name='Фотография', upload_to='products/')
-    name = models.CharField(verbose_name='Название', max_length=100)
-    description = models.TextField(verbose_name='Описание', blank=False)
-    price = models.PositiveIntegerField(verbose_name='Цена')
-    created_at = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name='Время редактирования', auto_now=True)
-    is_published = models.BooleanField(verbose_name='Опубликован', default=True)
-    product_category = models.ForeignKey(verbose_name='Категория', to='Category', on_delete=models.PROTECT)
-    product_subcategory = models.ForeignKey(verbose_name='Подкатегория', to='SubCategory', on_delete=models.PROTECT,
-                                            null=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        db_table = 'products'
-        ordering = ['-created_at']
 
 
 class Category(models.Model):
@@ -69,4 +48,31 @@ class SubCategory(models.Model):
         verbose_name = 'Подкатегория'
         verbose_name_plural = 'Подкатегории'
         db_table = 'subcategories'
+        ordering = ['-created_at']
+
+
+class Product(models.Model):
+    photo = models.ImageField(verbose_name='Фотография', upload_to='products/')
+    name = models.CharField(verbose_name='Название', max_length=100)
+    description = models.TextField(verbose_name='Описание', blank=False)
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    created_at = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Время редактирования', auto_now=True)
+    is_published = models.BooleanField(verbose_name='Опубликован', default=True)
+    product_category = models.ForeignKey(verbose_name='Категория', to='Category', on_delete=models.PROTECT, null=True)
+    product_subcategory = ChainedForeignKey(
+        to=SubCategory,
+        chained_field='product_category',
+        chained_model_field='subcategory_category',
+        show_all=False,
+        auto_choose=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        db_table = 'products'
         ordering = ['-created_at']
