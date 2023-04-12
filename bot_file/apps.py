7e399105580c082afdf328e18666a85d1b03dede
ddosm <ddosmukhambetov@gmail.com>
2023-04-12 22:24:17 +0600

    Добавлены комментарии

--- bot_file/apps.py
+++ bot_file/apps.py
@@ -4,4 +4,6 @@ from django.apps import AppConfig
 class BotFileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bot_file'
+
+    # verbose_name отображение в Django админке
     verbose_name = 'Продукты'
