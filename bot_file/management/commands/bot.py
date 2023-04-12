9d63a95002528cceb2a231bed562cd3ead6783b4
ddosm <ddosmukhambetov@gmail.com>
2023-04-12 14:56:46 +0600

    Сделан обработчик текста на который бот не может дать ответ

--- bot_file/management/commands/bot.py
+++ bot_file/management/commands/bot.py
@@ -1,6 +1,7 @@
 from django.core.management import BaseCommand
-from aiogram import executor
+from aiogram import executor, types
 from bot_file.handlers import default_handlers_register, catalog_handlers_register, authorization_handlers_register
+from bot_file.keyboards import default_kb
 from bot_file.loader import dp
 
 
@@ -14,4 +15,11 @@ class Command(BaseCommand):
         default_handlers_register()
         catalog_handlers_register()
         authorization_handlers_register()
+
+        @dp.message_handler(commands=None, regexp=None)
+        async def unknown_text(message: types.Message):
+            await message.answer("Простите, но я не понимаю вас ☹️\n\n"
+                                 "Попробуйте использовать команду Помощь ⭐️",
+                                 reply_markup=default_kb.only_help_markup)
+
         executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
